393f84d0733ba1e9146e1f18965cbf9c069a377d
SlayerX06 <148194662+SlayerX06@users.noreply.github.com>
2025-10-23 11:34:21 +0300

    oyun güncellemesi başarılı

---
+++ dosya işlemleri/reading file.py
@@ -0,0 +1,3 @@
+with open('text.txt') as file:
+    print(file.read()) # text.txt dosyasını aç ve içeriğini oku ve yazdır
+    #bu dosya uygulama ile aynı klasörde olmalıdır
--- zindanoyunu.py
+++ zindanoyunu.py
@@ -1,20 +1,27 @@
 import random
 
-oyuncu = input("Merhaba oyuncu sınıfını seç: A:Şövalye, B:Okçu, C:Büyücü ")
+oyuncu = input("""Merhaba oyuncu sınıfını seç:
+               A:Şövalye (Yüksek can, düşük hasar ve savunma yeteneği)
+               B:OKçu (Ortalama can, ortalama hasar, kafadan vuruş özelliği ile tek atma şansı)
+               C:Büyücü (Düşük can, yüksek hasar, büyü yeteneği, can yenileme) """)
 
 if oyuncu == "A":
     Can = 150
     Hasar = random.randint(20,30)
     Karizma = random.randint(1,10)
+    print("Şövalye seçtin! Canın: " + str(Can) + ", Hasarın: " + str(Hasar) + ", Karizman: " + str(Karizma))
 elif oyuncu == "B":
     Can = 100
     Hasar = random.randint(30,50)
     Karizma = random.randint(1,10)
+    print("Okçu seçtin! Canın: " + str(Can) + ", Hasarın: " + str(Hasar) + ", Karizman: " + str(Karizma))
 elif oyuncu == "C":
     Can = 75
     Hasar = random.randint(40,60)
     Karizma = random.randint(1,10)
     Mana = 100
+    print("Büyücü seçtin! Canın: " + str(Can) + ", Hasarın: " + str(Hasar) + ", Karizman: " + str(Karizma) + ", Manan: " + str(Mana))
+
 
 Goblin = {"Can": 30, "Hasar": random.randint(5,10), "Karizma": random.randint(1,2)}
 Ork = {"Can": 50, "Hasar": random.randint(20,25), "Karizma": random.randint(1,5)}
@@ -33,20 +40,56 @@ elif enemy == Troll:
 elif enemy == Ejderha:
     enemyname = "Ejderha"
 
+enemymoves = random.randint(1,3)
+
 print("Karşına bir " + enemyname + " çıktı! Ne yapacaksın? ")
-eylem = input("A:Saldır, B:Savun, C:Konuş, D:Yetenek, E:Pes Et ")
+
+if oyuncu == "A":
+    eylem = input("A:Saldır, B:Savun, C:Gözlemle D:Konuş, E:Pes et")
+elif oyuncu == "B":
+    eylem = input("A:Saldır, B:Kafadan vuruş, C:Gözlemle D:Konuş, E:Pes et")
+elif oyuncu == "C":
+    eylem = input("A:Saldır, B:Büyü yap, C:Gözlemle D:Konuş, E:Pes et, F:İyileşme")
 
 while enemy["Can"] > 0 and Can > 0:
+    if enemymoves == 1:
+        Can -= enemy["Hasar"]
+        print("Düşman sana " + str(enemy["Hasar"]) + " hasar verdi. Kalan canın: " + str(Can))
+        enemymoves = random.randint(1,3)
+    elif enemymoves == 2:
+        print("Düşman savunma durumuna geçti ve bu tur hasar vermedi.")
+        enemymoves = random.randint(1,3)
+    elif enemymoves == 3:
+        print("Düşman sana saldırmak yerine seni gözlemliyor.")
+        enemymoves = random.randint(1,3)
     if eylem == "A":
         enemy["Can"] -= Hasar
         print("Düşmana " + str(Hasar) + " hasar verdin. Düşmanın kalan canı: " + str(enemy["Can"]))
         if enemy["Can"] > 0 and Can > 0:
             eylem = input("Şimdi ne yapacaksın? ")
-    elif eylem == "B":
+    elif eylem == "B" and oyuncu == "A":
         print("Savunma durumuna geçtin bu tur hasar almayacaksın.")
         if enemy["Can"] > 0 and Can > 0:
             eylem = input("Şimdi ne yapacaksın? ")
-    elif eylem == "C":
+    elif eylem == "B" and oyuncu == "B":
+        şans = random.randint(1,4)
+        if şans == 4:
+            enemy["Can"] = 0
+            print("isabetli bir atış yaptın ve düşmanını yendin!")
+        elif şans != 4:
+            print("İsabet ettiremedin. Düşman hasar almadı :(")
+            eylem = input("Şimdi ne yapacaksın?")
+    elif eylem == "B" and oyuncu == "C":
+        if Mana >= 20:
+            enemy["Can"] -= Hasar * 2
+            Mana -= 20
+            print("Büyü yaptın! Düşmana " + str(Hasar * 2) + " hasar verdin. Düşmanın kalan canı: " + str(enemy["Can"]))
+            if enemy["Can"] > 0 and Can > 0:
+               eylem = input("Şimdi ne yapacaksın? ")
+        else: 
+            print("Yeterli manan yok!")
+            eylem = input("Şimdi ne yapacaksın? ")
+    elif eylem == "D":
         if Karizma > enemy["Karizma"]:
             print("Düşman seninle dost oldu!")
             break
@@ -54,11 +97,14 @@ while enemy["Can"] > 0 and Can > 0:
             print("Düşman seni dinlemedi.")
             if enemy["Can"] > 0 and Can > 0:
                eylem = input("Şimdi ne yapacaksın? ")
-    elif eylem == "D" and oyuncu == "C":
+    elif eylem == "C":
+        print("Düşmanın canı: " + str(enemy["Can"]) + ", hasarı: " + str(enemy["Hasar"]) + ", karizması: " + str(enemy["Karizma"]))
+        eylem = input("Şimdi ne yapacaksın? ")
+    elif eylem == "F" and oyuncu == "C":
         if Mana >= 20:
-            enemy["Can"] -= Hasar * 2
+            Can += 30
             Mana -= 20
-            print("Büyü yaptın! Düşmana " + str(Hasar * 2) + " hasar verdin. Düşmanın kalan canı: " + str(enemy["Can"]))
+            print("İyileştin! Kalan canın: " + str(Can))
             if enemy["Can"] > 0 and Can > 0:
                eylem = input("Şimdi ne yapacaksın? ")
         else:
@@ -69,11 +115,6 @@ while enemy["Can"] > 0 and Can > 0:
         print("Pes ettin. Oyun bitti.")
         break
 
-    if enemy["Can"] > 0 and eylem != "B":
-        Can -= enemy["Hasar"]
-        print("Düşman sana " + str(enemy["Hasar"]) + " hasar verdi. Kalan canın: " + str(Can))
-    elif enemy["Can"] > 0 and eylem == "B":
-        print("Düşmanın vuruşunu engelledin!")
 
     if Can <= 0:
         print("Oyuncu öldü. Oyun bitti.")
